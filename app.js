6ee9d346f3ce66b7be8da976ec636e5195caec30
Behzad Hatami <BehzadHatami@BehzadHatami-2.local>
2017-06-15 01:07:41 +0430

    add location name as argument from node cmd

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var process= require('process');
+var yargs = require('yargs');
+
 var locationService = require('./geolocation.js');
 var weatherService=require('./weather.js');
 
+var locationName = yargs.argv._[0];
 
-locationService.getGeolocationDetails('tehran', (err,result)=>{
+locationService.getGeolocationDetails(locationName, (err,result)=>{
     if(err){
         console.log(err);
     }
